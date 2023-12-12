6cb88698d5a945ecb3f7b2f7e3285743ef568946
Indhiro <indhiroelsaday@gmail.com>
2023-12-12 17:34:48 +0800

    PESAN

--- config/config.js
+++ config/config.js
@@ -1,38 +1,39 @@
 require('dotenv').config()
-var mysql = require('mysql');
+const { Sequelize } = require("sequelize");
 
-var con = mysql.createConnection({
-    user: process.env.DB_USERNAME,
+const con = new Sequelize({
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
+    dialect: "mysql",
     port: process.env.DB_PORT,
-    host: process.env.DB_HOST
-});
+    host: process.env.DB_HOST,
+})
 
 console.log(con);
 
-con.connect(function (err) {
-    if (err) throw err.message;
-    let queryDb = `CREATE DATABASE IF NOT EXISTS db_restaurant`
-    con.query(queryDb, function(err, result, fields) {
-        if (err) console.log(2);
-    });
-    let queryTbMenu = `CREATE TABLE IF NOT EXISTS ${'`railway`'}.tb_menu (
-        id int NOT NULL AUTO_INCREMENT,
-        nama varchar(255) NOT NULL,
-        harga int NOT NULL,
-        kategori enum ('makanan ringan', 'makanan berat', 'minuman') NOT NULL,
-        stok int NOT NULL,
-        deskripsi text NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP,
-        deleted_at TIMESTAMP,
-        PRIMARY KEY(id)
-    )`
-    con.query(queryTbMenu, function(err, result, fields) {
-        if (err) console.log(3);
-    });
-    console.log("Database connected!");
-})
+// con.connect(function (err) {
+//     if (err) throw err.message;
+//     let queryDb = `CREATE DATABASE IF NOT EXISTS db_restaurant`
+//     con.query(queryDb, function(err, result, fields) {
+//         if (err) console.log(2);
+//     });
+//     let queryTbMenu = `CREATE TABLE IF NOT EXISTS ${'`railway`'}.tb_menu (
+//         id int NOT NULL AUTO_INCREMENT,
+//         nama varchar(255) NOT NULL,
+//         harga int NOT NULL,
+//         kategori enum ('makanan ringan', 'makanan berat', 'minuman') NOT NULL,
+//         stok int NOT NULL,
+//         deskripsi text NOT NULL,
+//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//         updated_at TIMESTAMP,
+//         deleted_at TIMESTAMP,
+//         PRIMARY KEY(id)
+//     )`
+//     con.query(queryTbMenu, function(err, result, fields) {
+//         if (err) console.log(3);
+//     });
+//     console.log("Database connected!");
+// })
 
 module.exports = con;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const router = require('./routes/router');
+const sequelize = require('./config/config');
 const express = require('express');
 require('dotenv').config()
 const app = express();
@@ -7,4 +8,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router)
 
+sequelize.sync()
 app.listen(3000 || process.env.PORT, () => console.log('Server berjalan di port 3000' + "..."));
